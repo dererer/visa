fb8abd2db891aa56a71c37406e394ee95fa723d1
dererer <v-lviv@yandex.ru>
2016-04-01 06:45:47 -0400

    create solution
    create console project and add rucaptcha library

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,39 @@
+﻿using mevoronin.RuCaptchaNETClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RuCaptchaClient capClient = new RuCaptchaClient("a8033d2fa483a2d3b9fa0c001417f672");
+
+            string capId = capClient.UploadCaptchaFile("C://test.jpg");
+
+            for (int i = 0; i < 20 * 3000; i++)
+            {
+                try
+                {
+                    Console.WriteLine("Result -> " + capClient.GetCaptcha(capId));
+
+                    break;
+                }
+
+                catch
+                {
+                    System.Threading.Thread.Sleep(3000);
+                }
+            }
+            
+
+            Console.WriteLine("Balence -> " + capClient.GetBalance());
+
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ RuCaptchaClient/Run.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mevoronin.RuCaptchaNETClient
+{
+    class Run
+    {
+            //StartBrowserAndNavigateToURL("http://www.polandvisa-ukraine.com/scheduleappointment_2.html");
+
+            ////
+            //browserWindow.DomLinkDomClick("//A[@id='ctl00_plhMain_lnkSchApp']", "", 50 * 1000);
+            ////
+            //browserWindow.DomListBoxSelect("//SELECT[@id='ctl00_plhMain_cboVAC']", "Польщі Львів", "", 50 * 1000);
+            //browserWindow.DomListBoxSelect("//SELECT[@id='ctl00_plhMain_cboPurpose']", "Подача документів", "", 50 * 1000);
+            //browserWindow.DomButtonDomClick("//INPUT[@id='ctl00_plhMain_btnSubmit']");
+            ////
+
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomElementMouseMove("//DIV[@class='rc-anchor-normal-footer']");
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomElementMouseMove("//DIV[@class='rc-inline-block']");
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomElementMouseMove("//SPAN[@class='recaptcha-checkbox*']");
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomElementDomClick("//SPAN[@class='recaptcha-checkbox*']");
+            //System.Threading.Thread.Sleep(500);
+
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomElementMouseMove("//SELECT[@id='ctl00_plhMain_cboVisaCategory']");
+            //browserWindow.DomListBoxSelect("//SELECT[@id='ctl00_plhMain_cboVisaCategory']", "Шенгенська Віза", "");
+            //System.Threading.Thread.Sleep(500);
+            //browserWindow.DomButtonDomClick("//INPUT[@id='ctl00_plhMain_btnSubmit']");
+
+    }
+}
